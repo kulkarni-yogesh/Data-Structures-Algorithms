63e7c0eefebc8405cdae228920632b0ff8a2f7b1
Yogesh Kulkarni <ykulkarn@opentext.com>
2021-06-28 18:41:29 +0530

    first commit

---
+++ src/Arrays/TestArray.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		System.out.println("Factorial is: ");
+		System.out.println(factorial(5));
+
+	}
+
+	public static int factorial(int number) {
+		if (number == 0)
+			return 1;
+
+		return number * factorial(number - 1);
+
+	}
+
+}
